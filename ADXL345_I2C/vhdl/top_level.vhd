8dbb70295f06b4e239268f201b2ba8f5be571caf
bsilva <brunorafael223@gmail.com>
2017-02-23 19:16:17 +0000

    Init Project

--- ADXL345_I2C/vhdl/top_level.vhd
+++ ADXL345_I2C/vhdl/top_level.vhd
@@ -5,16 +5,29 @@ USE IEEE.STD_LOGIC_UNSIGNED.ALL;
 ENTITY top_level IS
 PORT 
 (
-	CLOCK_50: IN STD_LOGIC;
-
-	I2C_SCLK: OUT STD_LOGIC;
-	I2C_SDAT: INOUT STD_LOGIC;
-	
-	LED  : OUT STD_LOGIC_VECTOR(7 DOWNTO 0)
-	
+	CLOCK_50: IN 		STD_LOGIC;
+	I2C_SCLK: OUT 		STD_LOGIC;
+	I2C_SDAT: INOUT	STD_LOGIC;
+	LED	  : OUT 		STD_LOGIC_VECTOR(7 DOWNTO 0)
 );
 END top_level;
 
 ARCHITECTURE behavior OF top_level IS
-begin
+
+-- ====================================================
+--						     COMPONENTS
+-- ====================================================
+
+COMPONENT ADXL345_I2C is 
+	PORT(
+			 clk 	: in std_logic;
+			 reset : in std_logic;
+	);
+END COMPONENT;
+-- ====================================================
+--								SIGNAL
+-- ====================================================
+
+BEGIN
+
 END behavior;
